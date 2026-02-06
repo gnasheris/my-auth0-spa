1b141b8157453d6d4537766938555df3278077d4
gnasheris <nashi10327@gmail.com>
2026-02-06 13:37:17 +1100

    added fast api files & auth0 page

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience,
+        scope: "openid profile email",
+      }}
+      cacheLocation="localstorage"
+      useRefreshTokens
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>,
+);
+
